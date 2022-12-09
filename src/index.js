e0742a864dfbf3684eec39a8377c941d301f2f28
fan-droide <jose.gilpanal@telecom-paris.fr>
2022-12-09 11:54:26 +0100

    fix POST request

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ function createButton() {
     btn.innerHTML = 'LOGIN'
     btn.setAttribute('class', 'login-button')
     btn.setAttribute(
-        'href', 
-        'https://freesound.org/apiv2/oauth2/authorize/?client_id='+process.env.CLIENT_ID+'&response_type=code&state=xyz'
+        'href',
+        'https://freesound.org/apiv2/oauth2/authorize/?client_id=' + process.env.CLIENT_ID + '&response_type=code&state=xyz'
     )
-    
+
     document.getElementById('error').appendChild(btn)
 }
 
@@ -17,31 +17,16 @@ const urlParams = new URLSearchParams(queryString);
 const theCode = urlParams.get('code')
 
 
-if (theCode !==null) {
-    /* OPTION 1 */
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", 'https://freesound.org/apiv2/oauth2/access_token/', true);    
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    xhr.send(JSON.stringify({
-        client_id: process.env.CLIENT_ID,
-        client_secret: process.env.CLIENT_SECRET,                
-        code: theCode,
-        grant_type: 'authorization_code'
-    }));
-     /* OPTION 2 */
-    fetch('https://freesound.org/apiv2/oauth2/access_token/', {
-        method: 'POST',   
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(
-            {
-                client_id: process.env.CLIENT_ID,
-                client_secret: process.env.CLIENT_SECRET,                
-                code: theCode,
-                grant_type: 'authorization_code'
-            })
+if (theCode !== null) {
+
+    /* OPTION 3 */
+    fetch("https://freesound.org/apiv2/oauth2/access_token/?client_id=" 
+            + process.env.CLIENT_ID 
+            + "&client_secret=" 
+            + process.env.CLIENT_SECRET 
+            + "&grant_type=authorization_code&code=" 
+            + theCode + "", {
+        method: "POST"
     }).then(response => response.json()).then(response => console.log(JSON.stringify(response)))
 }
 createButton()
